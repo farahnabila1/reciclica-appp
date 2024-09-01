df8247ccd4f8dd859b1b2d8f51713493a66d4d25
farahnabila1 <farahnabilahaibah@mail.ugm.ac.id>
2024-09-01 23:22:10 +0700

    acara2

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyASWCkcM5XhrppTxFGMCdIk5OxFzu8EDTk",
+    authDomain: "reciclica-494a5.firebaseapp.com",
+    projectId: "reciclica-494a5",
+    storageBucket: "reciclica-494a5.appspot.com",
+    messagingSenderId: "407227258457",
+    appId: "1:407227258457:web:1b21158588be94e6200348",
+    measurementId: "G-MQ9PT1S2N8"
+  }
+};
